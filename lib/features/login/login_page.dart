ea95f29bfd0dc7f2df06c886218637f241b68984
SoopahMan <raviafiansyah22@gmail.com>
2024-10-22 23:25:33 +0700

    add login dan register

---
+++ lib/features/login/login_page.dart
@@ -0,0 +1,150 @@
+import 'package:face_count/configs/theme.dart';
+import 'package:flutter/material.dart';
+import 'register_page.dart'; 
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+          child: Column(
+            children: <Widget>[
+              // Bagian Logo dan Selamat Datang
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/logo.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Selamat Datang! 👋',
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Masuk ke FaceCount untuk memulai perjalanan Anda bersama kami!',
+                textAlign: TextAlign.center, // Teks berada di tengah
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              const SizedBox(height: 40),
+
+              // Bagian Form Login
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Alamat Email',
+                        hintText: 'Masukkan alamat email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Masukkan password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 15),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          // Aksi untuk lupa password
+                        },
+                        child: const Text(
+                          'Lupa Password?',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              
+              const SizedBox(height: 20),
+
+              // Bagian Tombol Login
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Aksi login
+                    }
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    backgroundColor: Color(0xFF0A4FF6),
+                  ),
+                ),
+              ),
+              
+              const SizedBox(height: 20),
+
+              // Tombol untuk Navigasi ke Halaman Daftar
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('Belum punya akun?', 
+                  style: TextStyle(color: neutral400)),
+                  TextButton(
+                    onPressed: () {
+                      // Navigasi ke halaman RegisterScreen
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const RegisterScreen()),
+                      );
+                    },
+                    child: const Text(
+                      'Daftar',
+                      style: TextStyle(color: primaryBase),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'features/acara/tambah_acara.dart';
 import 'features/beranda/beranda_page.dart';
 import 'features/kalender/kalender_page.dart';
-import 'features/profil/profil_page.dart';
+// import 'features/profil/profil_page.dart';
 import 'features/riwayat/riwayat_page.dart';
+import 'features/login/login_page.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -22,7 +23,7 @@ class _MainScreenState extends State<MainScreen> {
     const BerandaPage(),
     const KalenderPage(),
     const RiwayatPage(),
-    const ProfilPage(),
+    const LoginScreen(),
   ];
 
   @override
